9f31be38b98bfe6dc9d2b2be2e453f74727d4db7
Claire Nessler <clrnessler@gmail.com>
2015-08-31 11:25:41 -0600

    moved CORS function

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', process.env.LINK);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -27,13 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router);
 app.use('/api', api);
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', process.env.LINK);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-get.router("/", function (res, req, next) {
+router.get("/", function (res, req, next) {
   res.json({ message: "hello"});
 })
 
