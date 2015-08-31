f9f3c9f027403373c301e7755149d9eed8dcc60d
Claire Nessler <clrnessler@gmail.com>
2015-08-31 11:36:06 -0600

    removed public

--- public/angular/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', ['ngAnimate']);
-
-// app.config(function ($routeProvider) {
-//   $routeProvider
-//     .when("/", {
-//       templateUrl: "/partials/posts.html",
-//       controller: "ApplicationController"
-//     })
-// })
